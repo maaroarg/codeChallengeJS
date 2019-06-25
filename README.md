a0ade7e5f7a0fe5a8eba648213c8e6d2aa3c0554
Mario Romero <maaroarg@gmail.com>
2019-06-25 17:21:16 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Code Challenge!
+
+Desafío de código HTML/JS
+
+## Objetivo
+Enviar al servidor tus datos para que pueda contactarme contigo.
+
+## Cómo descargar el proyecto?
+Usando git, seguí estos pasos
+
+1) git clone nombre del repo o descargalo en ZIP.
+2) Listo! Podes usar un servidor web pero como es una SPA podes abrir el index.html directo en el navegador.
+
+## Y después?
+Lee las instrucciones que están en el archivo index.html completá y debugeá el código hasta lograr enviarme tu email y tu nombre.
+
+## Y después, después?
+Te voy a comunicar los resultados. Éxitos!
+Mario Romero
---
+++ js/index.js
@@ -0,0 +1,67 @@
+/****************
+  Complete the code! Good Luck
+******************************/
+
+const urlFirstStep = "https://us-central1-verto-backend-dev.cloudfunctions.net/challengeFirstStep";
+
+//Module pattern
+(function(){
+
+  //Module data
+  let urlLastStep, idLastStep;
+
+  //DOM
+  const btnFirstStep = document.querySelector("");
+  const btnLastStep = document.querySelector("");
+  const txtEmail = document.querySelector("input#emilio");
+  const txtName = document.querySelector("input");
+  const txtMsg = document.querySelector("span#msg");
+
+  //Events
+  btnFirstStep.addEventListener("click", gotoFirstStep);
+  btnLastStep.addEventListeners("click", );
+
+  //Callbacks
+  function gotoFirstStep(){
+
+    //First Step. Using promises
+    fetch(urlFirstStep, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify({
+        token: window.appToken,
+        email: txtEmail.value
+      })
+    })
+    .then(response=>response.json())
+    .then(data=>{
+      console.log("FromServer", data);
+      btnLastStep.disabled = false;
+    });
+
+  }
+
+  function gotoLastStep(){
+
+    //Last Step. Using promises
+    fetch(urlLastStep, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify({
+        name: txtName.value,
+        id: idLastStep
+      })
+    })
+    .then(response=>response.json())
+    .then(data=>{
+      console.log("FromServer LastStep", data);
+      txtMsg.innerText = data.data;
+    });
+  }
+})()
